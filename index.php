854cbbaf85f766cf841366eeaeb460209db5409e
Abdul Khalaque <limonbabu165@gmail.com>
2021-10-25 07:02:02 +0600

    phptest

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Way to Success</title>
+</head>
+<body>
+    <?php 
+     for($a = 1; $a <=100; $a++){
+        echo $a . ") I love PHP <br>";
+     }
+     
+     
+    ?>
+</body>
+</html>
